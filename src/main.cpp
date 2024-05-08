6d9c1aa0774d3de5a8d25280ea0243c2bb2f46d6
afsantos3 <afsantos369@gmail.com>
2024-05-08 18:00:48 -0400

    Init

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//#include <GLFW/glfw3.h>
+/*
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+*/
+int main() {
+  /*glfwSetErrorCallback(error_callback);
+
+  if (!glfwInit())
+  {
+    return -1;
+  }
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Dying Sun Set Editor", NULL, NULL);
+  if (!window)
+  {
+      // Window or OpenGL context creation failed
+  }
+
+  glfwTerminate();
+  */
+}
